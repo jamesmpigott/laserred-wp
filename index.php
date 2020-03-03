c006a88fc2da24aa388ee52f7c82792e70a5e25a
James Pigott <jmpigott96@gmail.com>
2020-03-03 23:26:40 +0000

    Index page gets current post ID and renders base layout outputting post content.

--- index.php
+++ index.php
@@ -2,41 +2,7 @@
 
 	$context = Timber::context();
 
-	$newsArgs = [
-		'post_type' => 'post',
-		'post_per_page' => 2,
-		'orderby' => [
-			'date' => 'DESC'
-		]
-	];
-	$context['news'] = Timber::get_posts($newsArgs);
+	$context['post'] = Timber::get_post(get_the_ID());
 
-	$productsArgs = [
-		'post_type' => 'product',
-		'posts_per_page' => 4,
-		'meta_key' => 'total_sales',
-		'orderby' => 'meta_value_num',
-	];
-	$context['products'] = Timber::get_posts($productsArgs);
-
-	$currentDate = date('Y-m-d H:i:s');
-	$eventArgs = [
-		'post_type' => 'events',
-		'posts_per_page' => 1,
-		'meta_key' => 'start_date',
-		'orderby' => 'start_date',
-		'order' => 'ASC',
-		'meta_query' => [
-			[
-				'key' => 'start_date',
-				'value' => $currentDate,
-				'compare' => '>=',
-				'type' => 'DATE',
-			],
-		],
-	];
-
-	$context['events'] = Timber::get_posts($eventArgs);
-	
-	Timber::render('pages/home.html.twig', $context);
+	Timber::render('base.html.twig', $context);
 ?>
