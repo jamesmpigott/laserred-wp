1b84cd29ab8a2bbfca572f10022426d66a04db90
James Pigott <jmpigott96@gmail.com>
2020-03-03 22:06:05 +0000

    Add attributes to newsletter cta shortcode.

--- shortcodes/newsletter-cta.php
+++ shortcodes/newsletter-cta.php
@@ -2,6 +2,14 @@
 add_shortcode( 'newsletter_cta', 'newsletter_cta' );
 
 function newsletter_cta( $atts ) {
+	$attributes = shortcode_atts( [
+		'title' => '',
+		'subtitle' => ''
+	], $atts);
+
 	$context = Timber::context();
-    return Timber::compile( 'partial/newsletter-cta.html.twig', $context );
+    return Timber::compile( 'partial/newsletter-cta.html.twig', [
+    	'context' => $context,
+    	'atts' => $atts
+    ]);
 }
